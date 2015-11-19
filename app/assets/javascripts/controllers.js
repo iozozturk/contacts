7ba4faf082011d6b9e11f671790825292d5724aa
ismet <iozozturk@gmail.com>
2015-11-19 23:56:33 +0200

    Autocomplete basic frontend impl

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -3,6 +3,10 @@ angular.module('contacts.controllers', ['contacts.services']).
 controller('ContactCtrl', function ($scope, $http, contactService) {
     'use strict';
 
+    $scope.message = "";
+    $scope.query = "";
+    $scope.contacts = [];
+
     $scope.uploadFile = function (files) {
         var fd = new FormData();
         //Take the first selected file
@@ -12,12 +16,29 @@ controller('ContactCtrl', function ($scope, $http, contactService) {
             withCredentials: true,
             headers: {'Content-Type': undefined},
             transformRequest: angular.identity
-        }).success(function(){
+        }).success(function (response) {
+            $scope.message = response;
             console.log("success");
-        }).error(function(){
+        }).error(function (error) {
+            $scope.message = response;
             console.log("error")
         });
+    };
 
+    $scope.search = function () {
+        if ($scope.query == "") {
+            $scope.contacts = [];
+        } else {
+            $http.get("/contacts", {
+                params: {q: $scope.query}
+            }).success(function (response) {
+                $scope.contacts = response;
+                console.log("success");
+            }).error(function (error) {
+                $scope.contacts = response;
+                console.log("error")
+            });
+        }
     };
 
 
