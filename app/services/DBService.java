f6b8c3f76de23b2807ad16bbc137b09ce2306c08
ismet <iozozturk@gmail.com>
2015-11-22 16:11:22 +0200

    MongoDb connection parameters are moved to conf

--- app/services/DBService.java
+++ app/services/DBService.java
@@ -1,37 +1,52 @@
 package services;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import play.Play;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ismet on 15/11/15.
  */
 public class DBService {
 
-    private static final String DB_NAME =  Play.application().configuration().getString("mongodb.name");
+    private static final String DB_NAME = Play.application().configuration().getString("mongodb.name");
+    private static final String USER = Play.application().configuration().getString("mongodb.name");
+    private static final String PASS = Play.application().configuration().getString("mongodb.pass");
+    private static final String HOST = Play.application().configuration().getString("mongodb.host");
+    private static final int PORT = Play.application().configuration().getInt("mongodb.port");
+
+    static MongoCredential credential = MongoCredential.createCredential(USER, DB_NAME, PASS.toCharArray());
+    static List<MongoCredential> credentials = new ArrayList<>();
+
     static final Morphia morphia = MorphiaClient.morphia;
     static final Datastore datastore = morphia.createDatastore(getMongoClient(), DB_NAME);
 
-    static{
+    static {
         morphia.mapPackage("models");
         datastore.ensureIndexes();
+        credentials.add(credential);
     }
 
     private static class MongoDBClient {
-       private static final MongoClient mongoClient = new MongoClient();
+        static ServerAddress address = new ServerAddress(HOST, PORT);
+        private static final MongoClient mongoClient = new MongoClient(address, credentials);
     }
 
     private static class MorphiaClient {
         private static final Morphia morphia = new Morphia();
     }
 
-    public static MongoClient getMongoClient(){
+    public static MongoClient getMongoClient() {
         return MongoDBClient.mongoClient;
     }
 
-    public static Datastore getMorphiaClient(){
+    public static Datastore getMorphiaClient() {
         return datastore;
     }
 
